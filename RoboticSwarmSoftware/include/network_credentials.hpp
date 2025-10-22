480b29f7baed90347579c4d8acee280923d51f10
AlwaysAbia <sandro.sulaberidze@gmail.com>
2025-10-22 14:57:03 +0400

    Ported GUI configuration to MQTT instead of HTTP

--- RoboticSwarmSoftware/include/network_credentials.hpp
+++ RoboticSwarmSoftware/include/network_credentials.hpp
@@ -1,6 +1,7 @@
 #ifndef NETWORK_CREDENTIALS_HPP
 #define NETWORK_CREDENTIALS_HPP
 
+//LAN Network Credentials
 const char* ssid = "";
 const char* password = "";
 
@@ -8,7 +9,7 @@ const char* hostname = "esp32_s3_2";
 const char* otapassword = "sampleOTAPass";
 
 //MQTT Credentials
-const char* mqtt_broker = "mqtt-broker.local";
+const char* mqtt_broker = "";
 const int mqtt_port = 1883; //Not using TLS
 const char* mqtt_user = "";
 const char* mqtt_password = "";
